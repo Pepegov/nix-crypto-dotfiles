9db3f4f460a59620d5061226fdd6ccd738439cf2
pepegov <andelismore@gmail.com>
2026-09-26 17:13:59 +0300

    Add git

--- nixos/modules/git.nix
+++ nixos/modules/git.nix
@@ -3,7 +3,7 @@
 {
   programs.git = {
     enable = true;
-    settings.user.name = "pepegov";
-    settings.user.email = "andelismore@gmail.com";
+    config.user.name = "pepegov";
+    config.user.email = "andelismore@gmail.com";
   };
 }
