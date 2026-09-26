27a203500708741657b3430e1a863ab8834be2a1
pepegov <andelismore@gmail.com>
2026-09-26 17:08:46 +0300

    Add git

--- nixos/hosts/crypto-vm/configuration.nix
+++ nixos/hosts/crypto-vm/configuration.nix
@@ -9,7 +9,7 @@
     ../../modules/desktop.nix
     ../../modules/trezor.nix
     ../../modules/browser.nix
-    ../../modulrs/git.nix
+    ../../modules/git.nix
   ];
 
   networking.hostName = "crypto-vm";
